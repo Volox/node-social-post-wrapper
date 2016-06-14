9396707dae9c5c122f986d4899ea5e85c4ef8428
Riccardo Volonterio <voloxxx@gmail.com>
2016-06-14 12:29:41 +0200

    feat: Added facebook

---
+++ src/wrappers/facebook.ts
@@ -0,0 +1,56 @@
+'use strict';
+// Load system modules
+
+// Load modules
+
+// Load my modules
+import { Wrapper, Post } from '../type';
+
+// Constant declaration
+
+// Module variables declaration
+
+// Module interfaces declaration
+interface FacebookPost extends Post {
+}
+
+// Module functions declaration
+function wrapFacebook( fbPost: any ): Post {
+  let tags: string[] = [];
+
+  const date: Date = new Date( fbPost.created_time );
+
+  const post: FacebookPost = {
+    id: fbPost.id,
+    text: fbPost.message,
+    date: date,
+    timestamp: date.getTime(),
+    author: fbPost.from.name,
+    authorId: fbPost.from.id,
+    tags: tags,
+
+    // Optional
+    location: null,
+
+    raw: fbPost,
+  };
+
+  if( fbPost.place && fbPost.place.location ) {
+    const lat: number = fbPost.place.location.latitude;
+    const lon: number = fbPost.place.location.longitude;
+    const location: GeoJSON.Point = {
+      type: 'Point',
+      coordinates: [ lat, lon ],
+    };
+  }
+
+  return post;
+}
+// Module class declaration
+
+// Module initialization (at first load)
+
+// Module exports
+export = wrapFacebook;
+
+//  50 6F 77 65 72 65 64  62 79  56 6F 6C 6F 78
